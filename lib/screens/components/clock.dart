cb05afb6c72c2b0afe6c299240c082ecc0134f4d
salsabileslam <salsabilelshiekh6@gmail.com>
2023-12-25 07:29:47 +0200

    Change theme

--- lib/screens/components/clock.dart
+++ lib/screens/components/clock.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_clock/models/my_theme_provider.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 import '../../constants.dart';
 import '../../size_config.dart';
@@ -28,29 +31,52 @@ class _ClockState extends State<Clock> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    offset: const Offset(0, 0),
-                    color: kShadowColor.withOpacity(0.14),
-                    blurRadius: 64),
-              ]),
-          child: Transform.rotate(
-            angle: -pi / 2,
-            child: CustomPaint(
-              painter: ClockPainter(context, dateTime),
+    return Stack(
+      children: [
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: const Offset(0, 0),
+                        color: kShadowColor.withOpacity(0.14),
+                        blurRadius: 64),
+                  ]),
+              child: Transform.rotate(
+                angle: -pi / 2,
+                child: CustomPaint(
+                  painter: ClockPainter(context, dateTime),
+                ),
+              ),
             ),
           ),
         ),
-      ),
+        Positioned(
+            top: 50,
+            left: 0,
+            right: 0,
+            child: Consumer<MyThemeModel>(
+              builder: (context, theme, child) => GestureDetector(
+                onTap: () {
+                  theme.changeTheme();
+                },
+                child: SvgPicture.asset(
+                  theme.isLightTheme
+                      ? "assets/icons/Sun.svg"
+                      : "assets/icons/Moon.svg",
+                  width: 24,
+                  height: 24,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ))
+      ],
     );
   }
 }
